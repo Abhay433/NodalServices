efdd20bc8da29e1769c1caadd01a7dee1d6d38c2
Abhay433 <abhayn433@gmail.com>
2025-10-12 17:12:40 +0530

    added testimonal feature and fixed email and phone number

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router'; // 👈 add this
+import { TestimonialsComponent } from '../../testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink], // 👈 add this
+  imports: [RouterLink,TestimonialsComponent], // 👈 add this
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'] // 👈 also fix to styleUrls (plural)
 })
